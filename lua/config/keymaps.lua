5161452a2bf2331eda124401b5f55ec54cd77624
Carlos Sanchez <carlos.moreno@headspace.com>
2026-01-06 11:31:04 -0800

    feat: Navigating features to have a pycharm like experience

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,24 @@
 
 -- jj in insert mode goes to normal mode
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "gd", function()
+  require("snacks").picker.lsp_definitions()
+end, { desc = "Goto Definition (picker)" })
+
+vim.keymap.set("n", "<leader>ss", function()
+  Snacks.picker.lsp_workspace_symbols()
+end, { desc = "Search symbols (workspace / LSP)" })
+
+vim.keymap.set("n", "gd", function()
+  require("snacks").picker.lsp_definitions()
+end, { desc = "Goto Definition (picker)" })
+
+vim.keymap.set("n", "gr", function()
+  require("snacks").picker.lsp_references()
+end, { desc = "Find References (usages)" })
+
+-- Peek group
+vim.keymap.set("n", "<leader>pd", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
+vim.keymap.set("n", "<leader>pi", "<cmd>Lspsaga peek_implementation<CR>", { desc = "Peek Implementation" })
+vim.keymap.set("n", "<leader>pt", "<cmd>Lspsaga peek_type_definition<CR>", { desc = "Peek Type Definition" })
