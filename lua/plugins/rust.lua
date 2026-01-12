3373572d0a23526a79e729accd5d517c2dd6eceb
Carlos Sanchez <carlos.sanchez@heru-app.com>
2026-01-11 18:04:09 -0600

    feat: Configure rust interpreter and disabled relative lines

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = { allFeatures = true },
+              checkOnSave = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
