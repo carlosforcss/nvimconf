547709797c09ddb9a392da242b07e9c8f2d4d0ef
Carlos Sanchez <carlos.moreno@headspace.com>
2026-04-27 14:50:34 -0700

    Inprove usage for colorblind people

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      background = { dark = "mocha" },
+      styles = {
+        comments = { "italic" },
+        keywords = { "bold" },
+        functions = { "bold" },
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        mini = { enabled = true },
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        treesitter = true,
+        telescope = { enabled = true },
+        which_key = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-mini/mini.files",
+  opts = {
+    windows = {
+      width_focus = 20,
+      width_preview = 20,
+    },
+  },
+}
